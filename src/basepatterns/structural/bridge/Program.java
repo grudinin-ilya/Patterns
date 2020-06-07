4b75d87298b5675a0307db8070d02c2d47aabcbf
ilya <grudinin_ilya@gmail.com>
2020-06-08 00:37:45 +0300

    Patterns/Bridge

---
+++ src/basepatterns/structural/bridge/Program.java
@@ -0,0 +1,10 @@
+package basepatterns.structural.bridge;
+
+public abstract class Program {
+    protected Developer developer;
+
+    public Program(Developer developer) {
+        this.developer = developer;
+    }
+    public abstract void developProgram();
+}
